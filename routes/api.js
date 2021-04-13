4c2ace31899cd11306846a4af1ef0be900253269
Glimpz <abgabbard@gmail.com>
2021-04-13 11:33:44 -0700

    updated with provided code from logan in class

--- routes/api.js
+++ routes/api.js
@@ -15,24 +15,40 @@ router.post("/api/workouts", ({ body }, res) => {
 
 
 router.get("/api/workouts", (req, res) => {
-  Workouts.find({})
-    .sort({ date: -1 })
-    .then(dbWorkouts => {
-      res.json(dbWorkouts);
+  Workouts.aggregate([
+    {
+      $addFields: {
+        totalDuration: {
+          $sum: '$exercises.duration',
+        },
+      },
+    },
+  ])
+    .then(dbTransaction => {
+      res.json(dbTransaction);
     })
     .catch(err => {
       res.status(400).json(err);
     });
 });
-
-router.get("/api/workouts/range", (req, res) => {
-  Workouts.find({})
-    .sort({ date: -1 })
-    .then(dbWorkouts => {
+router.get('/api/workouts/range', (req, res) => {
+  Workouts.aggregate([
+    {
+      $addFields: {
+        totalDuration: {
+          $sum: '$exercises.duration',
+        },
+      },
+    },
+  ])
+    .sort({ _id: -1 })
+    .limit(7)
+    .then((dbWorkouts) => {
+      console.log(dbWorkouts);
       res.json(dbWorkouts);
     })
-    .catch(err => {
-      res.status(400).json(err);
+    .catch((err) => {
+      res.json(err);
     });
 });
 
@@ -42,7 +58,8 @@ router.put("/api/workouts/:id", (req, res) => {
   }).catch(err => {
     res.status(400).json(err);
   })
-})
+});
+
 
 
 module.exports = router;
