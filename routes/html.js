e4935c5923fdb35367c20dd561ef2cc9620814d3
Glimpz <abgabbard@gmail.com>
2021-04-13 11:39:37 -0700

    added exercise and stats routes

--- routes/html.js
+++ routes/html.js
@@ -4,5 +4,13 @@ const { get } = require("./api");
 module.exports = function(app) {
     app.get("/", (req, res) => {
         res.sendFile(path.join(__dirname, "../public/index.html"));
+    });
+    app.get("/exercise", (req, res) => {
+        res.sendFile(path.join(__dirname, "../public/exercise.html"));
+    });
+    app.get("stats", (req, res) => {
+        res.sendFile(path.joinFile(__dirname, "../public/stats.html"))
     })
+
 }
+
