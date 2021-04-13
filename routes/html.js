76e61c681e1629ce32002a5d23ceb5b309174343
Glimpz <abgabbard@gmail.com>
2021-04-13 11:44:32 -0700

    autofill used joinFILE instead of join

--- routes/html.js
+++ routes/html.js
@@ -9,7 +9,7 @@ module.exports = function(app) {
         res.sendFile(path.join(__dirname, "../public/exercise.html"));
     });
     app.get("/stats", (req, res) => {
-        res.sendFile(path.joinFile(__dirname, "../public/stats.html"))
+        res.sendFile(path.join(__dirname, "../public/stats.html"))
     })
 
 }
